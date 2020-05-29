38f75bb1ef15be86c1b99539c879003b3c182be2
J <jennicc2@uci.edu>
2020-05-29 15:54:29 -0700

    test game html

--- public/js/main.js
+++ public/js/main.js
@@ -318,43 +318,6 @@ socket.on('game_update',function(payload){
 		return;
 	}
 
-	/* Update my color */
-	/*
-	if(socket.id == payload.game.player_white.socket){
-		my_color = 'white';
-	}
-	else if(socket.id == payload.game.player_black.socket){
-		my_color = 'black';
-	}
-	else{
-	*/
-		/* Something weird is going on, like three people playing at once */
-		/* send client back to the lobby */
-		/*
-		window.location.href = 'lobby.html?username='+username;
-		return;
-	}
-
-	$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
-	$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>');
-
-	clearInterval(interval_timer);
-	interval_timer = setInterval(function(last_time){
-		return function(){
-				var d = new Date();
-				var elapsedmilli = d.getTime() - last_time;
-				var minutes = Math.floor(elapsedmilli / (60 * 1000));
-				var seconds = Math.floor((elapsedmilli % (60 * 1000))/ 1000);
-
-				if(seconds < 10){
-					$('#elapsed').html(minutes+':0'+seconds);
-				}
-				else{
-					$('#elapsed').html(minutes+':'+seconds);
-				}
-		}}(payload.game.last_move_time)
-		, 1000);
-	*/
 
 	/* Animate changes to the board */
 
@@ -430,7 +393,6 @@ socket.on('game_update',function(payload){
 
 	old_board = board;
 });
-
 
 
 socket.on('play_token_response',function(payload){
