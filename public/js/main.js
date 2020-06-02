c2e1c38849aaaedbbf1be2b589bf523dca22ff33
jennicc2 <jennicc2@uci.edu>
2020-06-01 21:12:51 -0700

    woot end of reversi

--- public/js/main.js
+++ public/js/main.js
@@ -310,6 +310,7 @@ var old_board = [
 				];
 
 var my_color = ' ';
+var interval_timer = ' ';
 
 socket.on('game_update',function(payload){
 
@@ -343,8 +344,24 @@ socket.on('game_update',function(payload){
 			}
 
 			$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
+			$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>');
 
-
+			clearInterval(interval_timer);
+			interval_timer = setInterval(function(last_time){
+				return function(){
+					/* Do the work of updating the UI */
+					var d = new Date();
+					var elapsedmilli = d.getTime() - last_time;
+					var minutes = Math.floor(elapsedmilli / (60*1000));
+					var seconds = Math.floor((elapsedmilli % (60*1000))/1000);
+					if(seconds < 10){
+						$('#elapsed').html(minutes+':0'+seconds);
+					}
+					else{
+						$('#elapsed').html(minutes+':'+seconds);
+					}
+				}}(payload.game.last_move_time)
+				, 1000);
 	/* Animate changes to the board */
   var blacksum = 0;
 	var whitesum = 0;
@@ -390,10 +407,14 @@ socket.on('game_update',function(payload){
 				else{
 					$('#'+row+'_'+column).html('<img src="assets/images/error.gif" alt="error"/>');
 				}
+			}
 
 				/* Set up interactivity of the board */
 				$('#'+row+'_'+column).off('click');
-				if(board[row][column] == ' '){
+				$('#'+row+'_'+column).removeClass('hovered_over');
+
+				if(payload.game.whose_turn === my_color){
+					if(payload.game.legal_moves[row][column] === my_color.substr(0,1)){
 						$('#'+row+'_'+column).addClass('hovered_over');
 						$('#'+row+'_'+column).click(function(r,c){
 								return function(){
@@ -406,15 +427,12 @@ socket.on('game_update',function(payload){
 								};
 							}(row, column));
 						}
-            else{
-						    $('#'+row+'_'+column).removeClass('hovered_over');
-				    }
  	  	   }
   	   }
     }
 		$('#blacksum').html(blacksum);
 		$('#whitesum').html(whitesum);
-		
+
   	old_board = board;
 });
 
